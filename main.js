47f7e1246bfeb7c1705f28171a29243ccfecf80e
Wesley Chiou <wchiou1@umbc.edu>
2016-06-07 17:04:41 -0400

    again

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var version="redrawGraphs2";
+var version="redrawGraphs3";
 var canvas;
 var gl;
 var imageCanvas;
@@ -620,7 +620,7 @@ function drawGraphs(){
 		if(colorPanel==null){
 			continue;
 		}
-		clearRectangle(0,0,1200,600);
+		clearRectangle(receiveX+100,receiveY+receiveDelta*i-scaleHeight,250,600);
 		drawGraph(receiveX+100,receiveY+receiveDelta*i-scaleHeight,scaleWidth,scaleHeight,mapCIndices[i],setColorHeight[i]);//x,y,w,h,colorID, relative position(0 to 1)
 	}
 }
