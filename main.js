6221310b2c55f6eecacef2a2d8a0e094569ed2ba
Wesley Chiou <wchiou1@umbc.edu>
2016-06-13 19:30:41 -0400

    No border?

--- main.js
+++ main.js
@@ -984,7 +984,7 @@ function drawImgIcons(){
 	rectangle.changeColor(0.0,0.0,0.0);
 	rectangle.draw();
 	
-	clearRectangle(imgIconX,imgIconY+iconViewHeight,iconViewWidth,iconViewHeight);
+	//clearRectangle(imgIconX,imgIconY+iconViewHeight,iconViewWidth,iconViewHeight);
 	for(var i=0;i<img_panels.length;i++){
 			img_panels[i].changeColor(null);
 			img_panels[i].scale(iconWidth, iconHeight);
