4c89641f6d4dc326aa2f6ee4ab7da3b65f6de871
Wesley Chiou <wchiou1@umbc.edu>
2016-06-24 15:07:55 -0400

    Goddamn it...

--- colorform.js
+++ colorform.js
@@ -1,8 +1,7 @@
 var m1 = document.theForm.menu1;
 var m2 = document.theForm.menu2;
-
-var m3 = document.theForm.menu4;
-var m4 = document.theForm.menu5;
+var m4 = document.theForm.menu4;
+var m5 = document.theForm.menu5;
 function colorone2two() {
     m1len = m1.length ;
     for ( i=0; i<m1len ; i++){
@@ -35,32 +34,32 @@ function colortwo2one() {
 }
 
 function imgone2two() {
-    m3len = m3.length ;
-    for ( i=0; i<m3len ; i++){
-        if (m3.options[i].selected == true ) {
-            m4len = m4.length;
-            m3.options[m2len]= new Option(m3.options[i].text);
+    m4len = m4.length ;
+    for ( i=0; i<m4len ; i++){
+        if (m4.options[i].selected == true ) {
+            m5len = m5.length;
+            m4.options[m2len]= new Option(m4.options[i].text);
         }
     }
 
-    for ( i = (m3len -1); i>=0; i--){
-        if (m3.options[i].selected == true ) {
-            m3.options[i] = null;
+    for ( i = (m4len -1); i>=0; i--){
+        if (m4.options[i].selected == true ) {
+            m4.options[i] = null;
         }
     }
 }
 
 function imgtwo2one() {
-    m4len = m4.length ;
-        for ( i=0; i<m4len ; i++){
-            if (m4.options[i].selected == true ) {
-                m3len = m3.length;
-                m3.options[m3len]= new Option(m4.options[i].text);
+    m5len = m5.length ;
+        for ( i=0; i<m5len ; i++){
+            if (m5.options[i].selected == true ) {
+                m4len = m4.length;
+                m4.options[m4len]= new Option(m5.options[i].text);
             }
         }
-        for ( i=(m4len-1); i>=0; i--) {
-            if (m4.options[i].selected == true ) {
-                m4.options[i] = null;
+        for ( i=(m5len-1); i>=0; i--) {
+            if (m5.options[i].selected == true ) {
+                m5.options[i] = null;
             }
         }
 }
